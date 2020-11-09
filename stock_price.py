6b27df6e072375dd034be5b705c07590175de20d
Pritam Das <69068731+Pritam3355@users.noreply.github.com>
2020-11-09 08:34:28 +0530

    Update stock_price.py
    Formatted the data from starting date to end date

--- stock_price.py
+++ stock_price.py
@@ -2,9 +2,15 @@ from urllib.request import Request,urlopen
 from bs4 import BeautifulSoup 
 import csv,time,datetime
 from urllib.parse import urljoin
+import pandas as pd
 
 #url_g ='https://in.investing.com/commodities/gold-historical-data?end_date=1594578600&interval_sec=weekly&st_date=1531420200&interval_sec=daily'
 url_g = 'https://in.investing.com/commodities/gold-historical-data?end_date=1594578600&interval_sec=weekly&st_date=1436725800&interval_sec=daily'
+if os.path.exists('data.csv'):
+	os.remove('data.csv')
+	print('File Found,Deleted.')
+else:
+	pass
 
 f = open('data.csv','w',newline='')
 w = csv.writer(f)
@@ -21,4 +27,16 @@ for row in tbody.find_all('tr'):
 	else:
 		w.writerow(cols)
 
-print("Data stored in data.csv")
+df = pd.read_csv('data.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = df['Date'].dt.strftime("%d/%m/%Y")
+remove_ = ['Price','Open','High','Low']
+df[remove_] = df[remove_].replace({',':''},regex=True)
+df['Volume'] = df['Volume'].replace({'K':''},regex=True)
+df['Chg%'] = df['Chg%'].replace({'%':''},regex=True)
+df=df[::-1].reset_index()
+df = df[['Date','Price','Open','High','Low','Volume']]
+df.to_csv('gold_data.csv',index=False)
+
+print("Data stored in gold_data.csv")
+
